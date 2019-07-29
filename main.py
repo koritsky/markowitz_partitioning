07b031d85e92178193651a68638d09e01072d558
koritcky <koritcky@gmail.com>
2019-07-29 19:20:27 +0300

    Make functions clean and with comments

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import numpy as np
 import dimod
-from utilits import qubo_build, rand_sym_block_gen, split_until_threshold
-from hybrid.reference.kerberos import KerberosSampler
+from utilits import *
 
-around = 3
+# Choose number of digits after comma in console printout
+after_comma = 3
 
-# generate block matrix
-block_dim = [3, 5, 4]
-g_dim = sum(block_dim)
+
+# Generate random symmetric block matrix
+block_dim = [3, 1]
 matrix = rand_sym_block_gen(block_dim)
+g_dim = sum(block_dim)
 print("Given random block matrix:")
-print(np.around(matrix, around))
+print(np.around(matrix, after_comma))
 print("\n")
 """
 We define here (according to the letter) permutation matrix p_matrix as one 
@@ -20,34 +20,50 @@ Hence (P.T)AP is matrix A with permuted columns and rows.
 """
 
 
-# generate permutation matrix
+# Generate random permutation matrix
 p_matrix = np.random.permutation(np.eye(g_dim))
-print("Permutation matrix:")
+print("Random permutation matrix:")
 print(p_matrix)
 print("\n")
 
+# Mix generated matrix via permutation.
 mixed_matrix = np.dot(p_matrix.T, np.dot(matrix, p_matrix))
 print("Mixed matrix")
-print(np.around(mixed_matrix, around))
+print(np.around(mixed_matrix, after_comma))
 print("\n")
-qubo_matrix = qubo_build(mixed_matrix, theta=30)
+
+# Create binary quadratic model, that will find best permutation to return out mixed_matrix block structure
+qubo_matrix = qubo_build(mixed_matrix, theta=10)
 bqm = dimod.BinaryQuadraticModel.from_numpy_matrix(qubo_matrix, offset=2*g_dim)
-# response = dimod.ExactSolver().sample(bqm)
-response = KerberosSampler().sample(bqm)
-exact_solution = [int((i + 1) / 2) for i in response.first[0].values()]
-solution_permutation_matrix = np.asarray(exact_solution).reshape((g_dim, g_dim))
+
+# Get solution by brute force
+response = dimod.ExactSolver().sample(bqm)
+solution = [int((i+1)/2) for i in response.first[0].values()]
+solution_permutation_matrix = np.asarray(solution).reshape((g_dim, g_dim))
 print("Solution permutation matrix")
 print(solution_permutation_matrix)
 print("\n")
 
+# Check, whether solution matrix is permutation one.
+if not permutation_check(solution_permutation_matrix):
+    print("Solution is not permutation matrix")
+
+# Apply solution matrix to mixed and get the block one.
+new_matrix = np.dot(solution_permutation_matrix.T,
+                    np.dot(mixed_matrix,
+                           solution_permutation_matrix))
 print("New matrix")
-new_matrix = np.dot(solution_permutation_matrix.T, np.dot(mixed_matrix, solution_permutation_matrix))
-print(np.around(new_matrix, around))
+print(np.around(new_matrix, after_comma))
 print("\n")
+
+# Split new matrix into blocks with block dimension less then threshold
 print("Splitted matrices:")
-print("\n")
-for i in split_until_threshold(new_matrix, threshold=6):
-    print(np.around(i, around))
+for i in split_until_threshold(new_matrix, threshold=8):
+    print(np.around(i, after_comma))
+
+
+
+
 
 
 
--- utilits.py
+++ utilits.py
@@ -3,67 +3,55 @@ import itertools
 
 
 def qubo_build(cov_matrix, theta=1):
-    '''Create a qubo matrix by given covariance matrix
-
-    :param cov_matrix: covariance matrix from the problem. Must be symmetric.
-    :param theta: factor that determines how to penalize for violation of one-to-one permutation constrain.
-    Must be >0.
-    :return: QUBO matrix. Diagonal elements - linear coefficients. Non diagonal - quadratic.
-    Resulting QUBO matrix is upper diagonal
-    '''
-
-    # dimension of primary task
+    # Dimension of primary task
     n = cov_matrix.shape[0]
 
-    # tensor that goes into objective function, 4 variables
+    # Tensor that goes into objective function, 4 variables
     measure_tensor = np.zeros((n, n, n, n))
 
-    # matrix of qubo form
+    # Matrix of qubo form
     qubo_matrix = np.zeros((n ** 2, n ** 2))
 
-    # reshape tensor into qubo matrix
+    # Reshape tensor into qubo matrix
     for it in itertools.product([i for i in range(n)], repeat=4):
         i, j, k, l = it
         measure_tensor[i, j, k, l] = abs(cov_matrix[i, j]) * ((k-l) ** 2)
         qubo_matrix[i * n + k][j * n + l] = measure_tensor[i, j, k, l]
 
-    # add constraints
+    # Add constraints
     qubo_matrix = qubo_matrix + theta * ((-4)*np.eye(n ** 2)
                                          + np.kron(np.eye(n), np.ones((n, n)))
                                          + np.kron(np.ones((n, n)), np.eye(n)))
-    # make it upper-diagonal
-    # qubo_matrix = qubo_matrix + np.triu(qubo_matrix, 1) - np.tril(qubo_matrix, -1)
+    # Make it upper-diagonal
+    qubo_matrix = qubo_matrix + np.triu(qubo_matrix, 1) - np.tril(qubo_matrix, -1)
     return qubo_matrix
 
 
-def rand_sym_block_gen(block_dim:list):
-    '''Generates random symmetric block matrix
-    :param general_dim: dimension of the whole matrix
-    :param block_dim: dimension of  main block
-    :return:
-    '''
+def rand_sym_block_gen(block_dim: list):
+    # Check for valid block_dim list
     for i in block_dim:
         assert i > 0, "Nonpositive block dimension"
-    general_dim = sum(block_dim)
+
+    general_dim = sum(block_dim)  # Dimension of resulting matrix
     matrix = np.zeros((general_dim, general_dim))
+
     current_dim = 0
     for dim in block_dim:
         current_dim = current_dim + dim
-        block =  np.random.rand(dim, dim)
+        block = np.random.rand(dim, dim)
         matrix += matrix + np.pad(block,
                                   ((current_dim - dim, general_dim - current_dim),
                                    (current_dim - dim, general_dim - current_dim)),
                                   "constant",
                                   constant_values=(0, 0))
-    matrix = (matrix + matrix.T) / 2
+    # Make it symmetric
+    matrix = (1 / 2) * (matrix + matrix.T)
     return matrix
 
-def split_metric(matrix, split:int):
-    """Metric of matric splitting based on mean of non block elements
-    :param matrix:
-    :param split: index of last object in first part
-    :return: int, closer to zero - better split.
-    """
+
+def split_metric(matrix, split: int):
+    # By given split (after which column cut matrix) calculates metric - average value of non diagonal block elements.
+
     n = matrix.shape[0]
     m = split + 1
     assert n > m, "Split size more or equal to matrix size"
@@ -71,10 +59,12 @@ def split_metric(matrix, split:int):
     for row in range(m, n):
         for col in range(m):
             metric += abs(matrix[row, col])
-    return metric/((n - m) * m)
+    return metric / ((n - m) * m)
 
 
 def best_split(matrix):
+    # Calculates best split, that minimizes split_metric
+
     dim = matrix.shape[0]
     metric, split = 10 ** 3, 0
     for spl in range(dim - 1):
@@ -83,9 +73,11 @@ def best_split(matrix):
             metric, split = metr, spl
     return split
 
+
 def split_until_threshold(matrix, threshold):
-    mat_to_split = []
-    mat_to_split.append(matrix)
+    # Split matrix iteratively by 2 until each block has dimension less then threshold.
+
+    mat_to_split = [matrix]
     good_mat = []
     while len(mat_to_split) !=0:
         temp_mat_to_split = []
@@ -97,8 +89,13 @@ def split_until_threshold(matrix, threshold):
             lo_mat = mat[split :, split:]
             good_mat.append(lo_mat) if (mat.shape[0]-split) < threshold else temp_mat_to_split.append(lo_mat)
         mat_to_split.extend(temp_mat_to_split)
-
     return good_mat
+
+
+def permutation_check(matrix):
+    #Check, wheter given matrix is permutation one
+
+    return np.array_equal(np.dot(matrix, matrix.T), np.eye(matrix.shape[0]))
 
 
 
