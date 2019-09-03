23e660bbf7488728005e37bd770691bdf0e12345
Nikita <koritcky@gmail.com>
2019-09-03 18:28:38 +0300

    Create kerberos_investigate.py

--- runs/kerberos_investigate.py
+++ runs/kerberos_investigate.py
@@ -1,15 +1,30 @@
-from __future__ import print_function
-
-import sys
-
-import dimod
+from modules.partitioning import Partitioning
+import numpy as np
 from hybrid.reference.kerberos import KerberosSampler
 
+np.set_printoptions(precision=2,  # Digits after point
+                    linewidth=170,  # Length of the line
+                    suppress=True)  # Always fixed point notation (not scientific)
+
+block_dim = 334
+size = 9
 
 
-energy_threshold = None
-if len(sys.argv) > 2:
-    energy_threshold = float(sys.argv[2])
+def path(name):
+    return f"/home/koritskiy/rqc/Markowitz_partitioning/data/test_matrices/{block_dim}/{name}{block_dim}.csv"
 
-solution = KerberosSampler().sample(bqm, max_iter=10, convergence=3,
-                                    energy_threshold=energy_threshold)
+
+block_mat = np.genfromtxt(path("block_mat"), delimiter=",")
+mix_permutation_mat = np.genfromtxt(path("mix_permutation_mat"), delimiter=",")
+mixed_mat = np.genfromtxt(path("mixed_mat"), delimiter=",")
+part = Partitioning(mixed_mat, theta=500)
+
+response = KerberosSampler().sample(part.bqm, max_subproblem_size=60)
+solution = np.array([int(i) for i in response.first[0].values()])
+
+ordered_mat = part.permute(part.list_to_mat(solution))
+print(ordered_mat)
+
+
+
+
